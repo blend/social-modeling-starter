cde7e1f24eb52a80a1ad227622139f3e78395c94
Eugene Marinelli <eugeneiiim@gmail.com>
2012-05-17 22:04:20 -0700

    peopleloader cleanup

--- src/main/scala/com/blendlabsinc/process/PeopleLoader.scala
+++ src/main/scala/com/blendlabsinc/process/PeopleLoader.scala
@@ -7,9 +7,13 @@ import com.blendlabsinc.schema.PersonHBaseCollection
 object PeopleLoader extends scala.App {
   val limitFriends = if (args.length == 1) args(0).toInt else 1000
 
-  val people = Person(id = "me", name = "Me", likes = Graph.getLikes("me")) :: Graph.getFriendsWithLikes(limitFriends = limitFriends)
-  people.foreach { person =>
+  val people =
+    Person(id = "me", name = "Me", likes = Graph.getLikes("me")) ::
+    Graph.getFriendsWithLikes(limitFriends = limitFriends)
+
+  for (person <- people) {
     PersonHBaseCollection.put(person)
-    println(" - " + person.name + " (id=" + person.id + ") in " + person.likes.length + " likes")
+    println(" - %s (id=%s) in %s likes"
+            format (person.name, person.id, person.likes.length))
   }
 }
