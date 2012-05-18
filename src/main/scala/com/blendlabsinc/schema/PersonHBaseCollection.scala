f79f6676e7762edb9082c1323bf2a764a661d52e
Eugene Marinelli <eugeneiiim@gmail.com>
2012-05-18 11:33:25 -0700

    personhbasecollection cleanup

--- src/main/scala/com/blendlabsinc/schema/PersonHBaseCollection.scala
+++ src/main/scala/com/blendlabsinc/schema/PersonHBaseCollection.scala
@@ -3,8 +3,6 @@ package com.blendlabsinc.schema
 import com.blendlabsinc.models.Person
 
 object PersonHBaseCollection {
-  val Me = "me"
-
   def put(person: Person) =
     PersonSchema.PersonTable
       .put(person.id)
@@ -12,11 +10,12 @@ object PersonHBaseCollection {
       .valueMap(_.like, person.likes.map(like => like.id -> like.name).toMap)
       .execute()
 
-  def me = get(Me).getOrElse(throw new Exception("me not found."))
-
   def get(id: String): Option[Person] =
     PersonSchema.PersonTable.query2
       .withKey(id)
       .singleOption()
       .map(_.toPerson)
+
+  val Me = "me"
+  def me = get(Me).getOrElse(throw new Exception("me not found."))
 }
