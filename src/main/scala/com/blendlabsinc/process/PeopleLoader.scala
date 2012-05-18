d384550ee4fbcde30492ce39c4e6b104a5a3d54e
Quinn Slack <qslack@qslack.com>
2012-05-17 19:25:50 -0700

    PeopleLoader inserts "me" as well

--- src/main/scala/com/blendlabsinc/process/PeopleLoader.scala
+++ src/main/scala/com/blendlabsinc/process/PeopleLoader.scala
@@ -1,11 +1,13 @@
 package com.blendlabsinc.process
 
 import com.blendlabsinc.facebook.Graph
+import com.blendlabsinc.models.Person
 import com.blendlabsinc.schema.PersonHBaseStore
 
 object PeopleLoader extends scala.App {
-  Graph.getFriendsWithGroups().foreach { friend =>
-    PersonHBaseStore.put(friend)
-    println(" - " + friend.name + " (id=" + friend.id + ")")
+  val people = Person(id = "me", name = "Me", groups = Graph.getGroups("me")) :: Graph.getFriendsWithGroups()
+  people.foreach { person =>
+    PersonHBaseStore.put(person)
+    println(" - " + person.name + " (id=" + person.id + ") in " + person.groups.length + " groups")
   }
 }
