9443ce030eee6a7a02f0947d63a9ee9d15d2cad2
Eugene Marinelli <eugeneiiim@gmail.com>
2012-05-17 18:30:04 -0700

    run the hadoop job

--- src/test/scala/com/blendlabsinc/mapreduce/CommonGroupsJobSpec.scala
+++ src/test/scala/com/blendlabsinc/mapreduce/CommonGroupsJobSpec.scala
@@ -1,10 +1,30 @@
 package com.blendlabsinc.mapreduce
 
 import org.specs2.mutable._
+import org.apache.hadoop.hbase.HBaseConfiguration
 
 class CommonGroupsJobSpec extends Specification {
   "Common groups job" should {
     "calculate common groups" in {
+      val settings = new CommonGroupsJobSettings
+
+      val config = {
+        val c = HBaseConfiguration.create
+        val localConfigPaths = List("conf/hbase-site.xml", "../conf/hbase-site.xml")
+        localConfigPaths.foreach((p) => {
+          val path = new org.apache.hadoop.fs.Path(p)
+          if ((new java.io.File(path.toString)).exists) {
+            c.addResource(path)
+          }
+        })
+        c
+      }
+
+      new CommonGroupsJob().run(
+        settings,
+        config
+      )
+
       "" must equalTo("")
     }
   }
