c37397848b278a3cb34e89b74c6cec373fd80349
Eugene Marinelli <eugeneiiim@gmail.com>
2012-05-17 22:00:43 -0700

    graph.scala cleanup

--- src/main/scala/com/blendlabsinc/facebook/Graph.scala
+++ src/main/scala/com/blendlabsinc/facebook/Graph.scala
@@ -12,22 +12,20 @@ import com.blendlabsinc.models._
 
 object Graph {
   private val accessTokenEnvName = "FACEBOOK_ACCESS_TOKEN"
-  private def accessToken = {
-    val a = java.lang.System.getenv(accessTokenEnvName)
-    if (a == null) {
+
+  private def accessToken =
+    Option(System.getenv(accessTokenEnvName)).getOrElse(
       throw new Exception(
-        "The " + accessTokenEnvName +" environment variable is not set. To get your Facebook access token, go to " +
+        "The " + accessTokenEnvName +" environment variable is not set. To get " +
+        "your Facebook access token, go to " +
         "https://developers.facebook.com/tools/explorer?method=GET&path=me " +
         "and use the value in the \"Access Token:\" field. Then run " +
         "`export " + accessTokenEnvName + "=\"your access token\"` " +
         "in your shell."
-      )
-    } else {
-      a
-    }
-  }
+      ))
 
-  private implicit val formats = Serialization.formats(FullTypeHints(List(classOf[Person], classOf[Like])))
+  private implicit val formats =
+    Serialization.formats(FullTypeHints(List(classOf[Person], classOf[Like])))
 
   private def apiURL(id: String, path: String, limit: Int, fields: String) =
     dispatch.url("https://graph.facebook.com/" + id + "/" + path) <<? Map(
